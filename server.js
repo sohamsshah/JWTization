f9f98726a203eb1d0d98533bdcf7a8f357ddc784
sohamsshah <47717492+sohamsshah@users.noreply.github.com>
2020-06-09 12:05:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
+// A Web service for JWT Token Authentication
+
+/* 
+Dependancies: 
+    Runtime Environment: Node.js  
+    Back-end Framework: Express 
+    Database: MongoDB
+    Middleware: Passport.js
+*/
+
 var express = require("express");
 var bodyParser = require("body-parser");
 app = express();
-var morgan = require("morgan");
-var mongoose = require("mongoose");
-var cors = require("cors");
+var morgan = require("morgan"); //Logger Middleware function
+var mongoose = require("mongoose"); //MongoDB Object Modelling Tool
+var cors = require("cors"); //for cross origin possibilities
 var passport = require("passport");
 var config = require("./config/database");
 var port = process.env.PORT || 3000;
+
+// Configuring our application
+
 mongoose.connect(config.db, 
     { useMongoClient: true });
 
 mongoose.Promise = global.Promise;
 require('./config/passport')(passport);
-
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
@@ -22,7 +33,7 @@ app.use(cors());
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static("public"));
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); //Comment this out when you are using 
 
 
 require('./routes/routes_2')(app,passport); 
